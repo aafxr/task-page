e5e4072a587c653d1b6d421a7d3752297d7ea713
aafxr <alexandral.fed23@gmail.com>
2024-09-18 10:08:58 +0700

    auth

--- src/bitrix/bitrix.ts
+++ src/bitrix/bitrix.ts
@@ -1,22 +1,22 @@
-import axios from "axios";
 
 const HOST_NAME = process.env.REACT_APP_HOST_NAME || ''
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || ''
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET || ''
 
+const baseURL = `https://${HOST_NAME}`
 
-const axios_bx = axios.create({
-    baseURL: `https://${HOST_NAME}`,
-})
+
+// const axios_bx = axios.create({
+//     baseURL: `https://${HOST_NAME}`,
+// })
 
 
 export const bitrix = {}
 
 export async function getAuth() {
-    const res = await axios_bx.get(`/oauth/authorize/?client_id=${CLIENT_ID}&state=JJHgsdgfkdaslg7lbadsfg`).catch(console.error)
-    if (!res || res.status !== 200) return {}
-    const authResponse = res.data
+    const authResponse: any = await fetch(`${baseURL}/oauth/authorize/?client_id=${CLIENT_ID}&state=JJHgsdgfkdaslg7lbadsfg`).catch(console.error)
+    if (!authResponse) return {}
 
     console.log(authResponse)
     const params = Array.from(new URL(authResponse.url).searchParams.entries()).reduce((a, [k, v]) => {
@@ -31,7 +31,7 @@ export async function getAuth() {
     paramsForToken.append('client_secret',CLIENT_SECRET)
     paramsForToken.append('code',params.code)
 
-    const tokens = await axios_bx.get('/oauth/token/?' + paramsForToken.toString()).catch(console.error)
+    const tokens = await fetch(baseURL + '/oauth/token/?' + paramsForToken.toString()).catch(console.error)
 
     console.log(tokens)
     return tokens
