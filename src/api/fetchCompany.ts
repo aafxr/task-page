a22a0e275c4e197e89085c6cf4944e75ac2be733
aafxr <alexandral.fed23@gmail.com>
2024-10-18 11:22:00 +0700

    fix contact fetch api

--- src/api/fetchCompany.ts
+++ src/api/fetchCompany.ts
@@ -5,7 +5,7 @@ import {BASE_URL} from "../App";
 import axios from "axios";
 
 export async function fetchCompany(id: string){
-    const res = await appFetch.post<APiResponse<BXCompany>>(BASE_URL + 'api/contact/getById/', {id})
+    const res = await appFetch.post<APiResponse<BXCompany>>(BASE_URL + 'api/company/getById/', {id})
     if(res.status > 199 && res.status < 300){
         if (res.data.ok){
             return new BXCompany(res.data.result)
